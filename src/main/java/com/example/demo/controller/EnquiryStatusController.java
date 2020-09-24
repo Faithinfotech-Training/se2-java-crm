8656cb2d69d307ad06f4dd875be26d7d7f44ef8e
Ronit-j <pictronit007@gmail.com>
2020-09-24 09:17:53 +0530

    Update Manage Sales Resource Enquiry API

--- src/main/java/com/example/demo/controller/EnquiryStatusController.java
+++ src/main/java/com/example/demo/controller/EnquiryStatusController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import com.example.demo.entity.EnquiryStatus;
 import com.example.demo.service.CourseEnquiryService;
 import com.example.demo.service.EnquiryStatusService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/enquiry")
 public class EnquiryStatusController {
