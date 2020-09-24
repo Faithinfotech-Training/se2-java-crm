fc29a9e9d495d7f13411164331bc47dfea83d32e
AshishChavan98 <chavan.ashish98@gmail.com>
2020-09-24 16:54:23 +0530

    commiting local changes

--- src/main/java/com/example/demo/dao/ResourceEnquiryDAO.java
+++ src/main/java/com/example/demo/dao/ResourceEnquiryDAO.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-//Interface of the DAO Layer of Resource Enquiry
-package com.example.demo.dao;
-
-import java.util.List;
-
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.entity.ResourceEnquiry;
-
-@Repository
-public interface ResourceEnquiryDAO  {
-    //method to get all resource enquiries
-	List<ResourceEnquiry> findAll();
-	
-	//method to get a specific enquiry
-	ResourceEnquiry getOne(int resourceEnquiryId);
-	
-	//method to save an enquiry
-	void save(ResourceEnquiry resourceEnquiry);
-	
-	//method to delete a specific enquiry
-	ResourceEnquiry deleteByResourceEnquiryId(int resourceEnquiryId);
-
-//	Filter ResourceEnquiry by status
-	List<ResourceEnquiry> findAllResourceEnquiryByStatus(int status);
-
-	List<ResourceEnquiry> findAllResourceEnquiryByResourceType(int rescourceType);
-	
-	
-}
-=======
 //Interface of the DAO Layer of Resource Enquiry
 package com.example.demo.dao;
 
@@ -69,4 +37,3 @@ public interface ResourceEnquiryDAO  {
 	
 	
 }
->>>>>>> 5f19ed8f8d38ffa3256101883bb0c8f6c5603409
--- src/main/java/com/example/demo/service/ResourceEnquiryService.java
+++ src/main/java/com/example/demo/service/ResourceEnquiryService.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package com.example.demo.service;
-
-import java.util.List;
-
-import com.example.demo.entity.ResourceEnquiry;
-
-public interface ResourceEnquiryService {
-
-	
-	public List<ResourceEnquiry> findAllResourceEnquiry();
-	
-	
-	public ResourceEnquiry findByResourceEnquiryId(int theId);
-	
-	public void saveResourceEnquiry(ResourceEnquiry resourceEnquiry);
-	
-	public void updateResourceEnquiry(int resourceEnquiryId, ResourceEnquiry resourceEnquiry);
-	
-	public void deleteByResourceEnquiryId(int resourceEnquiryId);
-	
-	public List<ResourceEnquiry> findAllResourceEnquiryByStatus(int status);
-	
-	public List<ResourceEnquiry> findAllResourceEnquiryByResourceType(int resourceType);
-}
-=======
 package com.example.demo.service;
 
 import java.util.List;
@@ -50,4 +24,3 @@ public interface ResourceEnquiryService {
 	
 	
 }
->>>>>>> 5f19ed8f8d38ffa3256101883bb0c8f6c5603409
