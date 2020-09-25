dec81579853489c784080c1b54e74ac9729235ff
Trupti-Bhandari <truptibhandari09@gmail.com>
2020-09-25 12:31:15 +0530

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { ManagerComponent } from './manager/manager.component';
+import { AuthGaurdService } from './service/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'admin',component:AdminComponent,canActivate:[AuthGaurdService]},
+  {path:'login',component:LoginComponent},
+  {path:'manager', component:ManagerComponent,canActivate:[AuthGaurdService]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+
+export class AppRoutingModule {
+
+ }
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { LoginServiceService } from './service/login-service.service';
+import { AdminComponent } from './admin/admin.component';
+import { ManagerComponent } from './manager/manager.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [LoginServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
